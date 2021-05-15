d667c5a8b3af756fe7241199ad30363e703b78f8
Lean Rasmussen <leanmkrasmussen@gmail.com>
2021-05-15 11:12:07 +0200

    refator: test updated to fix refactor

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { mount, shallow } from 'enzyme';
 import App from './App';
 
-const rows = [
+const userData = [
   {
     name1: 'Mads L. Klausen',
     email: 'MadsLKlausen@jourrapide.com',
@@ -40,13 +40,13 @@ it('renders without crashing', () => {
 });
 
 it('renders 5 rows', () => {
-  const wrapper = mount(<App rows={rows} locale="da" rowsPerPage={5} />);
+  const wrapper = mount(<App userData={userData} locale="da" rowsPerPage={5} />);
 
   expect(wrapper.find('tr').length).toBe(5);
 });
 
 it('filters rows based on input', () => {
-  const wrapper = mount(<App rows={rows} locale="da" rowsPerPage={5} />);
+  const wrapper = mount(<App userData={userData} locale="da" rowsPerPage={5} />);
 
   wrapper.find('input').simulate('change', { target: { value: 'k' } });
 
